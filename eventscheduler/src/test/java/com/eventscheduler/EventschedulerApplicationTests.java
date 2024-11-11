e5524b069dd108df44956ec6808ce54f769e6b2e
PratikKedare24 <pratikkedare73@gmail.com>
2024-11-11 12:50:09 +0530

    add project structure

---
+++ eventscheduler/src/test/java/com/eventscheduler/EventschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
